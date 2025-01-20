0b96a3a7bcc046f787420c53ccb806d01712e4e5
@russkikh_anna <anna.russkih13@gmail.com>
2025-01-20 16:24:10 +0700

    awaiting added

--- src/test/java/org/example/TestDemoQA.java
+++ src/test/java/org/example/TestDemoQA.java
@@ -3,14 +3,18 @@ package org.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 
 public class TestDemoQA {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.navigate().to("https://demoqa.com/");
-        driver.findElement(By.xpath("//h5[contains(text(), 'Elements')]")).click();
-        //driver.findElement(By.cssSelector("h5:contains('Elements')")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//h5[contains(text(), 'Elements')]"))).click();
         driver.quit();
     }
 }
