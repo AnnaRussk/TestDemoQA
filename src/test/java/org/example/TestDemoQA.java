e1ccb404c9ef1df1b7aa4ed48cda562c6a400d1e
@russkikh_anna <anna.russkih13@gmail.com>
2025-01-23 21:02:58 +0700

    Before-after annotations added Check-box test with assertion

--- src/test/java/org/example/TestDemoQA.java
+++ src/test/java/org/example/TestDemoQA.java
@@ -11,7 +11,9 @@ import org.junit.jupiter.api.Test;
 
 class TestDemoQA {
 
-    private static WebDriver driver;
+    String url = "https://demoqa.com/";
+
+    public static WebDriver driver;
 
     @BeforeAll
     public static void setUp() {
@@ -22,25 +24,22 @@ class TestDemoQA {
 
     @Test
     public void textFieldTest() {
-        driver.navigate().to("https://demoqa.com/");
-        WebElement tab = driver.findElement(By.xpath("//h5[contains(text(), 'Elements')]"));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", tab);
-        tab.click();
+        driver.navigate().to(url);
+        WebElement tabElement = driver.findElement(By.xpath("//h5[contains(text(), 'Elements')]"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", tabElement);
+        tabElement.click();
         WebElement element = driver.findElement(By.xpath("//span[contains(text(), 'Text Box')]"));
         element.click();
-        //WebElement inputField = driver.findElement(By.xpath("//input[id='userName']"));
-        //inputField.sendKeys("Какое-то Имя");
-        //локатор точно правильный - но sendKeys не работает, тест останавливается на этом месте
+        WebElement inputField = driver.findElement(By.xpath("//input[@id='userName']"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", inputField);
+        inputField.sendKeys("Какое-то Имя");
         WebElement buttonSubmit = driver.findElement(By.xpath("//button[@id='submit']"));
         buttonSubmit.click();
     }
 
     @Test
     public void checkBoxTest() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setPageLoadStrategy(PageLoadStrategy.EAGER);
-        WebDriver driver = new ChromeDriver(chromeOptions);
-        driver.navigate().to("https://demoqa.com/");
+        driver.navigate().to(url);
         WebElement tab = driver.findElement(By.xpath("//h5[contains(text(), 'Elements')]"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", tab);
         tab.click();
@@ -48,10 +47,17 @@ class TestDemoQA {
         element.click();
         WebElement CheckBox = driver.findElement(By.xpath("//span[@class='rct-checkbox']"));
         CheckBox.click();
-
         String expectedText = "You have selected :";
         WebElement selectedTextElement = driver.findElement(By.xpath("//span[contains(text(), 'You have selected')]"));
         Assertions.assertEquals(expectedText, selectedTextElement.getText().trim());
+    }
+
+    @Test
+    public void formTest() {
+        driver.navigate().to(url);
+        WebElement tabForm = driver.findElement(By.xpath("//h5[contains(text(), 'Forms')]"));
+
+
     }
 
     @AfterAll
